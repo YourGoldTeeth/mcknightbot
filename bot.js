566c0fc85eb6c4b38ebf980a6d2bf161c94bbe8d
James West <jameseswest@gmail.com>
2018-01-11 19:04:46 -0600

    add sixth status array for the reset of the loop. add stream to reply to users who tweet back at one and then resets the count back at one

--- bot.js
+++ bot.js
@@ -9,25 +9,21 @@ var config = require('./config');
 // sets up new twit class using keys
 var T = new Twit(config);
 // // set up a user stream
-// var stream = T.stream('user');
+var stream = T.stream('user');
 
 // *****CODE*****
-
-// set interval to run tweetit function. time is in milliseconds.
-// 1000 is one s * 60 is one minute * 60 is one hour
-// setInterval(tweetIt, 1000*60*60)
 
 // SETTING STATUS VARIABLES
 var status_one = 'one, you\'re like a:'
 var status_two = 'two, just want to:'
-var status_three = 'three, girl it\'s plain to see that:'
-var status_four_one = 'four, repeat steps one through three. (one:)'
-var status_four_two = 'four, repeat steps one through three. (two:)'
-var status_four_three = 'four, repeat steps one through three. (three:)'
+var status_three = 'three, girl it\'s plain to see that you\'re the :'
+var status_four_one = 'four, repeat steps one through three. (one: you\'re like a:)'
+var status_four_two = 'four, repeat steps one through three. (two: just want to:)'
+var status_four_three = 'four, repeat steps one through three. (three: girl it\'s plain to see that:)'
 var status_five = 'five, make you fall'
-var status_six = 'i believe my job is done, now i\'ll start back at ooooooone. ye he ye yeah.'
+var status_six = 'i believe my job is done, now i\'ll start back at ooooooone. (ye he ye yeah).'
 
-var status_arr = [status_one, status_two, status_three, status_four_one, status_four_two, status_four_three, status_five]
+var status_arr = [status_one, status_two, status_three, status_four_one, status_four_two, status_four_three, status_five, status_six]
 
 // SETTING SEARCH VARIABLES
 var params_one = { 
@@ -39,7 +35,7 @@ var params_two = {
 }
 
 var params_three = { 
-	q: '"you\'re the only one for me"', 
+	q: '"only one for me"', 
 }
 
 var params_five = { 
@@ -48,46 +44,62 @@ var params_five = {
 
 var queries = [params_one, params_two, params_three, params_one, params_two, params_three, params_five]
 
+// SETS INITIAL COUNT FOR CYCLING THROUGH QUERY AND STATUS ARRAYS
 i = 0
 
-// TIME THE TWEETS
-setInterval(tweetIt, 1000*60*30)
+// ENABLES FOLLOWUP ACTION FOR MENTION OF SCREENNAME
+stream.on('tweet', tweetEvent)
+
+function tweetEvent(eventMsg) {
+
+	var replyto = eventMsg.in_reply_to_screen_name;
+	var text = eventMsg.text;
+	var from = eventMsg.user.screen_name
+
+	console.log('replyto: ' + replyto + ' from: ' + from + ' text: ' + text);
+
+	if (replyto === 'botatone' && text == '@botatone back at one') {
+		var replytweet = '@' + from + ' thank you for believing that my work is done. i will now start back at ooooooone. (ye he ye yeah) ';
+		T.post('statuses/update', {status: replytweet }, reset_i);
+	}
+	
+	function reset_i() {
+			i = 0
+	}
+}
+
+// TIME THE TWEETS FOR EVERY TWO HOURS
+setInterval(tweetIt, 1000*60*60*2)
 
 // SETS SEARCH/TWEET IN MOTION
 function tweetIt() {
 
-	T.get ('search/tweets', queries[i], parse);
+	if ( i <= 6) {
+		T.stream ('statuses/filter', { possibly_sensitive: false })
 
-	function parse(err, data, response) {
-		var tweets = data.statuses;
-		var rtId = tweets[0].id_str;
-		var rtUser = tweets[0].user.screen_name;
+		T.get ('search/tweets', queries[i], parse);
 
-			T.post('statuses/update', { status: status_arr[i] + ' https://twitter.com/' + rtUser + '/status/' + rtId }, add_i)
+		function parse(err, data, response) {
+			var tweets = data.statuses;
+			var rtId = tweets[0].id_str;
+			var rtUser = tweets[0].user.screen_name;
 
-			{
-				console.log(data);
-			};
+				T.post('statuses/update', { status: status_arr[i] + ' https://twitter.com/' + rtUser + '/status/' + rtId }, add_i)
 
-			function add_i() {
-				i = i + 1;
-
-				if (i == 7) {
-				    i = 0;
-				} 
-			}	
-	}
-}
-	// }
+				{
+					console.log(data);
+				};
+		}
+		function add_i() {
+			i++;
+		}		
+	} else
+		T.post('statuses/update', { status: status_arr[i] }, reset_i)
 		
-
-
-		// T.post('statuses/retweet/:id', { id: rtId } , function (err, data, response) {
-		// 		console.log(data)
-		// })
-		// for (var i = 0; i < tweets.length; i++) {
-		// console.log("\n" + 'number ' + i + "\n" + tweets[i].text) + "\n" + "\n";}
-// }
+		function reset_i() {
+			i = 0
+		}
+}
 
 
 
--- prac.js
+++ prac.js
@@ -17,60 +17,60 @@ var stream = T.stream('user');
 // 1000 is one ms * 60 is one minute * 60 is one hour
 setInterval(tweetIt, 1000*60*60)
 
-// *Follow*
-// (if this event happens, run this function)
-// stream.on('follow', followed);
+*Follow*
+(if this event happens, run this function)
+stream.on('follow', followed);
 
-// *Tweet*
-// (if this event happens, run this function)
-// stream.on('tweet', tweetEvent);
-
-
-// 
-// function followed(eventMsg) {
-// 	var name = eventMsg.source.name;
-// 	var screenName = eventMsg.source.screen_name;
-// 	tweetIt('@' + screenName + ', thanks for the follow! If you ever believe this bot\s work is done, then tweet \'@botatone back at one\' and I\'ll start all over again from one!')
-// }
+*Tweet*
+(if this event happens, run this function)
+stream.on('tweet', tweetEvent);
 
 
 
-// function tweetIt(txt) {
-// 	var tweet = {
-// 		status: txt
-// 	}
-
-	// T.post('statuses/update', tweet, tweeted)
-
-// 	function tweeted(err, data, response) {
-// 		if (err) {
-// 			console.log('uh oh error')
-// 		} else {
-// 			console.log('it worked!')
-// 		}
-// 	}
-// }	
+function followed(eventMsg) {
+	var name = eventMsg.source.name;
+	var screenName = eventMsg.source.screen_name;
+	tweetIt('@' + screenName + ', thanks for the follow! If you ever believe this bot\s work is done, then tweet \'@botatone back at one\' and I\'ll start all over again from one!')
+}
 
 
-// GET FUNCTIONS
-	// var params_one = {
-	// 	q: 'like a dream come true',
-	// 	count: 5
-	// }
 
-	// var params_two = {
-	// 	q: 'just want to be with you',
-	// 	count: 5
-	// }
+function tweetIt(txt) {
+	var tweet = {
+		status: txt
+	}
 
-	// console.log('one:')
-	// T.get('search/tweets', params_one, gotData);
+	T.post('statuses/update', tweet, tweeted)
 
-	// console.log('two:')
+	function tweeted(err, data, response) {
+		if (err) {
+			console.log('uh oh error')
+		} else {
+			console.log('it worked!')
+		}
+	}
+}	
 
-	// function gotData(err, data, response) {
-	// 	var tweets = data.statuses;
-	// 	for (var i = 0; i < tweets.length; i++) {
-	// 		console.log(tweets[i].text);
-	// 	}
-	// }
+
+GET FUNCTIONS
+	var params_one = {
+		q: 'like a dream come true',
+		count: 5
+	}
+
+	var params_two = {
+		q: 'just want to be with you',
+		count: 5
+	}
+
+	console.log('one:')
+	T.get('search/tweets', params_one, gotData);
+
+	console.log('two:')
+
+	function gotData(err, data, response) {
+		var tweets = data.statuses;
+		for (var i = 0; i < tweets.length; i++) {
+			console.log(tweets[i].text);
+		}
+	}
