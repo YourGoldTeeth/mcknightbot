4743ddf2be2e83b7c9595aeefae7c557d0c0dcb6
James West <jameseswest@gmail.com>
2018-02-06 14:54:02 -0600

    remove twitter keys

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  consumer_key:         'OgtmGKRIaBl11inrf7tgDoEzG',
-  consumer_secret:      'v0OBRjpOwfnsHfG2Tqa9JPFU34XOY7xnQYNsHkCD2jzYw2qPll',
-  access_token:         '717791905405394944-UbOugG2T0tm4sC2tCG96TEg3AbcRZMk',
-  access_token_secret:  'gOq4XEeNUsYrwN4WcT8HoqWt8rE886rW9THmJiMbzYScm',
+  consumer_key:         '',
+  consumer_secret:      '',
+  access_token:         '',
+  access_token_secret:  '',
   timeout_ms:           60*1000,
 }
