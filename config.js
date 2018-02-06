65d630aec91e5aeaefff2f1ef048618b42c00b47
James West <jameseswest@gmail.com>
2018-02-06 14:28:27 -0600

    initial commit

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  consumer_key:         'G1yFX1NvO0aaaouP0if2UgZ29',
-  consumer_secret:      'pzCrcVdXC26efsWSQHTJ8OM01xFES7LdQAAhUELyeLpdcxcXY6',
-  access_token:         '717791905405394944-r5zcvOTq2sop6SwUdTdm2oi0qTFoaeq',
-  access_token_secret:  'qlC4N6V45yyDRc8U6LvE3UJdk0VKxhffOPPVsJV42F4pb',
+  consumer_key:         '',
+  consumer_secret:      '',
+  access_token:         '',
+  access_token_secret:  '',
   timeout_ms:           60*1000,
 }
